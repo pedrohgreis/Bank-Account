6abad6e7c9ff21b4b6861334686112637158fb15
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-07-25 21:11:18 -0400

    Update account.ts

--- account.ts
+++ account.ts
@@ -2,4 +2,3 @@
 console.log("dou a bunda pra varios homens, chupo um monte de pinto, dou a bunda pros caras, eu vou bater o recorde mundial em dar a bunda, eu sou estrupado, eu sou um arrombado, eu dou a bunda pra 2050 homens diferentes, os cavalos comeram minha bunda");
 
 console.log("Hello");
-penis
